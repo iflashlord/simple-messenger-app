c83633a52bf518c8cd3510cc6d5090374a8cef93
pooladrag <me@iflashlord.com>
2020-12-13 15:36:39 +0330

    add click event on firend compnent pass user to props function

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -2,10 +2,21 @@
 import React from "react";
 
 class Friend extends React.Component {
-    
+
+    constructor() {
+        super();
+
+        this.onSelectFriend = this.onSelectFriend.bind(this);
+    }
+
+    onSelectFriend() {
+        this.props.select(this.props.user);
+    }
+
     render() {
         return (
             <button 
+                onClick={this.onSelectFriend}
                 className="flex flex-row items-center focus:outline-none hover:bg-gray-300 text-indigo-800 text-white hover:bg-indigo-600 hover:text-white focus:bg-indigo-600 focus:text-white rounded-xl p-2">
                 
                 <div className="flex items-center justify-center h-8 w-8 bg-indigo-200 rounded-full">
