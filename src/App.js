ec6b2e88622474e7b9b70458805cc3f0c78ac950
pooladrag <me@iflashlord.com>
2020-12-13 15:31:59 +0330

    add send message form component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import React from 'react'
 import MessagesList from "./components/MessagesList";
+import SendMessage from "./components/SendMessage";
 import FriendsList from "./components/FriendsList";
 
 class App extends React.Component {
@@ -24,29 +25,8 @@ class App extends React.Component {
                
               <MessagesList/>
   
-              <form className='flex flex-row items-center h-16  bg-white w-full px-4'>
-                <div className='flex-grow ml-0'>
-                  <div className='relative w-full'>
-                    <input type='text' placeholder='Type your message here...' className='flex w-full border-2 rounded-3xl focus:outline-none focus:border-indigo-800 pl-4 h-10' />
-                  </div>
-                </div>
-                <div className='ml-4'>
-                  <button type='submit' className='flex items-center justify-center h-10 w-10 focus:outline-none rounded-full bg-gray-200 hover:bg-gray-300 text-indigo-800 text-white hover:bg-indigo-600 hover:text-white focus:bg-indigo-600 focus:text-white'>
-                    <svg
-                      className='w-5 h-5 transform rotate-90 -mr-px'
-                      fill='none'
-                      stroke='currentColor'
-                      viewBox='0 0 24 24'
-                      xmlns='http://www.w3.org/2000/svg'>
-                      <path
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                        strokeWidth='2'
-                        d='M12 19l9 2-9-18-9 18 9-2zm0 0v-8'></path>
-                    </svg>
-                  </button>
-                </div>
-              </form>
+              <SendMessage/>
+
             </section>
           </aside>
         </section>
